f7ec4a7b8ce6e9ae6419c8d4489a2fe2d3213e57
RupeshPatel22 <rupeshpatel2607@gmail.com>
2025-05-06 18:41:49 +0530

    Feat: Authentication added and problem model added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,21 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.get("/", (req,res) => {
+    console.log(`Hello guys welcome to leetground🔥`);
+    
+})
+
+app.use("/api/v1/auth", authRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on Port: ${PORT}`);
